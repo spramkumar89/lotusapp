8e8546bad1c4ef677a8e918b3dcc497458565c24
Ramkumar <spramkumar89@gmail.com>
2023-05-11 13:19:37 +0800

    Initial project setup

---
+++ src/main/java/com/ram/dev/lotusapp/LotusappApplication.java
@@ -0,0 +1,14 @@
+package com.ram.dev.lotusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotusappApplication.class, args);
+		System.out.println("Lotusapp Started");
+	}
+
+}
---
+++ src/test/java/com/ram/dev/lotusapp/LotusappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.dev.lotusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
